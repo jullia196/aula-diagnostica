96faf341f2c5c404457f6cfafe024b723a5526fc
Jullia Alicia Silva Pereira - SENAI - Cuiaba/MT <00591949@alunosenai.mt>
2026-07-24 11:14:38 -0400

    commit 4

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ print("olá mundo!")
 nome = input("Digite seu nome: ")
 
 idade = int(input("Digite sua idade: "))
+
+n1 = int(input("Primeiro número: "))
+n2 = int(input("Segundo número: "))
+print("Soma:", n1 + n2)
